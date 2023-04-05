df5c3a2113ad5abe23ae86124748fdbcc519936e
Melissa Ost <melissa-ost@hotmail.com>
2023-04-05 16:59:17 -0300

    validacoes

---
+++ src/main/java/med/voll/api/domain/consulta/validacoes/ValidadorMedicoComOutraConsultaNoMesmoHorario.java
@@ -0,0 +1,24 @@
+package med.voll.api.domain.consulta.validacoes;
+
+import med.voll.api.domain.ValidacaoException;
+import med.voll.api.domain.consulta.DadosAgendamentoConsulta;
+import med.voll.api.domain.medico.MedicoRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Component;
+
+import java.time.Duration;
+import java.time.LocalDateTime;
+
+@Component
+public class ValidadorMedicoComOutraConsultaNoMesmoHorario implements ValidadorAgendamentoDeConsulta{
+
+    @Autowired
+    private MedicoRepository repository;
+
+    public void validar(DadosAgendamentoConsulta dados){
+        var medicoPossuiOutraConsutaNoMesmoHorario = repository.existsByMedicoIdAndData(dados.idMedico(), dados.data());
+        if (medicoPossuiOutraConsutaNoMesmoHorario){
+            throw new ValidacaoException("Medico já possui outra consulta agendada nesse horario");
+        }
+    }
+}
